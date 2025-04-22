a635f8db9e1e9e201e5ed5ba31d4203833139391
unknown <sw18@CESAEPRT.WAN>
2025-04-22 10:19:15 +0100

    primeiro commit

--- algoritmia/FichasPraticas/src/FichaPratica01/Ex02_MenosVar.java
+++ algoritmia/FichasPraticas/src/FichaPratica01/Ex02_MenosVar.java
@@ -3,13 +3,13 @@ package FichaPratica01;
 // Importar a biblioteca Scanner para ler os inputs do utilizador
 import java.util.Scanner;
 
-public class Ex02 {
+public class Ex02_MenosVar {
     public static void main(String[] args) {
-        // // Criação do Scanner - Vamos ler inputs do utilizador
+        // Criação do Scanner - Vamos ler inputs do utilizador
         Scanner input = new Scanner(System.in);
 
         // Declarar variáveis
-        int numero1, numero2, soma, subtracao, multi, divisao;
+        int numero1, numero2, resultado;
 
         // Ler numero 1
         System.out.print("Introduz um número:");
@@ -20,27 +20,27 @@ public class Ex02 {
         numero2 = input.nextInt();
 
         // Calcular a soma
-        soma=numero1+numero2;
+        resultado=numero1+numero2;
 
         //Apresentar a soma
-        System.out.println("Soma:"+soma);
+        System.out.println("Soma:"+resultado);
 
         // Calcular a subtracao
-        subtracao=numero1-numero2;
+        resultado=numero1-numero2;
 
         //Apresentar a subtracao
-        System.out.println("Subtração:"+subtracao);
+        System.out.println("Subtração:"+resultado);
 
         // Calcular a multiplicacao
-        multi=numero1*numero2;
+        resultado=numero1*numero2;
 
         //Apresentar a multiplicacao
-        System.out.println("Multiplicacao:"+multi);
+        System.out.println("Multiplicacao:"+resultado);
 
         // Calcular a divisao
-        divisao=numero1/numero2;
+        resultado=numero1/numero2;
 
         //Apresentar a divisao
-        System.out.println("Divisão:"+divisao);
+        System.out.println("Divisão:"+resultado);
     }
 }
--- algoritmia/FichasPraticas/src/FichaPratica01/Ex04.java
+++ algoritmia/FichasPraticas/src/FichaPratica01/Ex04.java
@@ -1,4 +1,32 @@
 package FichaPratica01;
 
+// Importar a biblioteca Scanner para ler os inputs do utilizador
+import java.util.Scanner;
+
 public class Ex04 {
+    public static void main(String[] args) {
+        // Criação do Scanner - Vamos ler inputs do utilizador
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        double raio, area, perimetro;
+
+        // Ler a area, que vai ser pedido pelo utilizador
+        System.out.print("Introduza o raio do círculo:");
+        raio = input.nextDouble();
+
+        // Calcular o perimetro
+        perimetro = 2 * raio * 3.14;
+
+        // Apresentar a area
+        System.out.println("Perimetro: " + perimetro);
+
+        // Calcular a area
+        area = Math.pow(raio, 2 ) * 3.14;
+
+        // Apresentar a area
+        System.out.println("Area: " + area);
+
+
+    }
 }
