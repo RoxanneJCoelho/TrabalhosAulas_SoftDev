a6eb60e2473ce524caa0b264bdfd2df339fc2a69
unknown <sw18@CESAEPRT.WAN>
2025-05-20 12:33:07 +0100

    alteracao pastas

---
+++ algoritmia/src/FichaPratica07/Ex01.java
@@ -0,0 +1,29 @@
+package FichaPratica07;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Ex01 {
+
+    /**
+     * Função que imprime o conteúdo de um ficheiro na consola
+     * @Params caminhoFicheiro - caminho para o ficheiro
+     * @Throws FileNotFoundExceptions - caso o ficheiro não exista
+     */
+
+    public static void imprimirFicheiro(String caminhoFicheiro) throws FileNotFoundException {
+        File ficheiro = new File (caminhoFicheiro);
+        Scanner sc = new Scanner (ficheiro);
+
+        while (sc.hasNextLine()){
+            String linha = sc.nextLine();
+            System.out.println(linha);
+        }
+    }
+
+    public static void main(String[] args) {
+        imprimirFicheiro(Ficheiros/FichaPratica07/exercicio_01.txt);
+
+    }
+}
