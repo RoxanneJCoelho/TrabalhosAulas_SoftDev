40bc5dbb872fecfd76196761859cecd41b7ba31c
unknown <sw18@CESAEPRT.WAN>
2025-06-30 16:46:15 +0100

    atualizado

--- DOM/js/teste.js
+++ DOM/js/teste.js
@@ -1,17 +1,36 @@
-let myForm = document.querySelector('form');
+// let myForm = document.querySelector('form');
 
-myForm.addEventListener('submit', function(e){
-  e.preventDefault()
-  console.log('cucu, faz validações e coisas de ui/ux, nomeadamente bloquear o botão');
+// myForm.addEventListener('submit', function(e){
+//   e.preventDefault()
+//   console.log('cucu, faz validações e coisas de ui/ux, nomeadamente bloquear o botão');
 
-  const animalVal = document.querySelector('#animal').value 
-  const myList = document.querySelector('#zoo-animals');
-  const myLi = document.createElement('li');
-  myLi.textContent = animalVal;
+//   const animalVal = document.querySelector('#animal').value 
+//   const myList = document.querySelector('#zoo-animals');
+//   const myLi = document.createElement('li');
+//   myLi.textContent = animalVal;
  
  
-  myList.appendChild(myLi)
+//   myList.appendChild(myLi)
  
-  myForm.reset();
-})
+//   myForm.reset();
+// })
+
+// funcao que faz a consulta á api, traz o resultado em json e transforma de forma a que o JS consiga interpretar
  
+const loadTVShow = async() => {
+  const result = await fetch('https://api.tvmaze.com/search/shows?q=friends');
+  const data = await result.json();
+
+  return data;
+}
+
+// funcao que constroi com o resultado da api (quando ele chegar) o html
+
+const getData = async() => {
+  const result = await loadTVShow();
+
+  // console log ou criar elementos, etc
+  console.log(result)
+
+}
+getData();
